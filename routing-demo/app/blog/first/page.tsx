c0dde73df69fc30335a8eb603ae998308683d39d
ANEENAKARUVATHILKOYA <aneenasaneer17@gmail.com>
2025-11-28 12:16:04 +0400

    routing and nested routing

---
+++ routing-demo/app/blog/first/page.tsx
@@ -0,0 +1,5 @@
+export default function First(){
+    return(
+        <h1>Blog first page !</h1>
+    )
+}
---
+++ routing-demo/app/blog/second/page.tsx
@@ -0,0 +1,5 @@
+export default function Second(){
+    return(
+        <h1>Blog second!!!</h1>
+    )
+}
