6617e6fa5c878cf6299510dc2e1b86db3f5d1eaa
ANEENAKARUVATHILKOYA <aneenasaneer17@gmail.com>
2025-11-30 11:06:09 +0400

    nextjs features file colocation and private folders

---
+++ routing-demo/app/_lib/page.tsx
@@ -0,0 +1,5 @@
+export default function Privateroute(){
+    return(
+        <h1>you cannot view this page in browser</h1>
+    )
+}
---
+++ routing-demo/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+function Barcode(){
+    return (
+        <h1>Bar code </h1>
+    )
+}
+
+
+export default function Dashboard() {
+    return (
+        <h1>dash board</h1>
+    )
+}
